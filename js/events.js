c737fde59b59c2a005baed5061c44eda89ed6cc1
Obi <obinnawalker@gmail.com>
2017-11-07 21:58:56 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -1,4 +1,31 @@
 //define functions here
+function getIt(){
+  $('p').on('click', function(){
+    alert('hey')
+  })
+}
+
+function frameIt(){
+  $('').on('load', function(){
+    
+  })
+}
+
+function pressIt(){
+  $('form').on('keydown', function(){
+    if($("input:first").val() === 71){
+      alert('alert you pressed G')
+      return
+    }  
+  })
+}
+
+function submitIt(){
+  $('form').on('submit', function(){
+    alert('Your form is going to be submitted now.')
+    return
+  })
+}
 
 $(document).ready(function(){
 
